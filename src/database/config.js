be2ee8b8e1e13aa6299f810cf9b39a038d304268
gab-braga <gabrielbraga0712@gmail.com>
2024-09-25 20:18:43 -0700

    fix: update .env file

--- src/database/config.js
+++ src/database/config.js
@@ -1,7 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-const connection = new Sequelize('finance_db', 'root', '123456', {
-    host: "127.0.0.1",
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+
+const connection = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
     dialect: 'mysql',
 });
 
